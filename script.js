a668b443ca896a1cf75fdb17fa66e3fe613a1cc9
Rohith M <rohithmerala@gmail.com>
2022-01-16 12:43:35 +0530

    V1.0

---
+++ script.js
@@ -0,0 +1,45 @@
+function function1() {
+    document.getElementById("audio1").play();
+    document.getElementById("audio2").pause();
+    document.getElementById("audio3").pause();
+    document.getElementById("audio4").pause();
+    document.getElementById("audio5").pause();
+    }
+
+function function2() {
+    document.getElementById("audio1").pause();
+    document.getElementById("audio2").play();
+    document.getElementById("audio3").pause();
+    document.getElementById("audio4").pause();
+    document.getElementById("audio5").pause();
+    }
+function function3() {
+    document.getElementById("audio1").pause();
+    document.getElementById("audio2").pause();
+    document.getElementById("audio3").play();
+    document.getElementById("audio4").pause();
+    document.getElementById("audio5").pause();
+    }
+function function4() {
+    document.getElementById("audio1").pause();
+    document.getElementById("audio2").pause();
+    document.getElementById("audio3").pause();
+    document.getElementById("audio4").play();
+    document.getElementById("audio5").pause();
+    }
+function function5() {
+    document.getElementById("audio1").pause();
+    document.getElementById("audio2").pause();
+    document.getElementById("audio3").pause();
+    document.getElementById("audio4").pause();
+    document.getElementById("audio5").play();
+    }
+
+
+function stop(){
+    document.getElementById("audio1").pause();
+    document.getElementById("audio2").pause();
+    document.getElementById("audio3").pause();
+    document.getElementById("audio4").pause();
+    document.getElementById("audio5").pause();
+    }
