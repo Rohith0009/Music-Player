85ba2f90ea00e3faa9b1c4bd82b7bdaea4b7ce47
Rohith M <rohithmerala@gmail.com>
2022-01-27 15:27:51 +0530

    Added More Songs!!

--- script.js
+++ script.js
@@ -1,45 +1,74 @@
 function function1() {
-    document.getElementById("audio1").play();
-    document.getElementById("audio2").pause();
-    document.getElementById("audio3").pause();
-    document.getElementById("audio4").pause();
-    document.getElementById("audio5").pause();
-    }
+  document.getElementById("Song1").play();
+  document.getElementById("Song2").pause();
+  document.getElementById("Song3").pause();
+  document.getElementById("Song4").pause();
+  document.getElementById("Song5").pause();
+  document.getElementById("Song6").pause();
+}
 
 function function2() {
-    document.getElementById("audio1").pause();
-    document.getElementById("audio2").play();
-    document.getElementById("audio3").pause();
-    document.getElementById("audio4").pause();
-    document.getElementById("audio5").pause();
-    }
+  document.getElementById("Song1").pause();
+  document.getElementById("Song2").play();
+  document.getElementById("Song3").pause();
+  document.getElementById("Song4").pause();
+  document.getElementById("Song5").pause();
+  document.getElementById("Song6").pause();
+  document.getElementById("Song7").pause();
+}
 function function3() {
-    document.getElementById("audio1").pause();
-    document.getElementById("audio2").pause();
-    document.getElementById("audio3").play();
-    document.getElementById("audio4").pause();
-    document.getElementById("audio5").pause();
-    }
+  document.getElementById("Song1").pause();
+  document.getElementById("Song2").pause();
+  document.getElementById("Song3").play();
+  document.getElementById("Song4").pause();
+  document.getElementById("Song5").pause();
+  document.getElementById("Song6").pause();
+  document.getElementById("Song7").pause();
+}
 function function4() {
-    document.getElementById("audio1").pause();
-    document.getElementById("audio2").pause();
-    document.getElementById("audio3").pause();
-    document.getElementById("audio4").play();
-    document.getElementById("audio5").pause();
-    }
+  document.getElementById("Song1").pause();
+  document.getElementById("Song2").pause();
+  document.getElementById("Song3").pause();
+  document.getElementById("Song4").play();
+  document.getElementById("Song5").pause();
+  document.getElementById("Song6").pause();
+  document.getElementById("Song7").pause();
+}
 function function5() {
-    document.getElementById("audio1").pause();
-    document.getElementById("audio2").pause();
-    document.getElementById("audio3").pause();
-    document.getElementById("audio4").pause();
-    document.getElementById("audio5").play();
-    }
+  document.getElementById("Song1").pause();
+  document.getElementById("Song2").pause();
+  document.getElementById("Song3").pause();
+  document.getElementById("Song4").pause();
+  document.getElementById("Song5").play();
+  document.getElementById("Song6").pause();
+  document.getElementById("Song7").pause();
+}
+function function6() {
+  document.getElementById("Song1").pause();
+  document.getElementById("Song2").pause();
+  document.getElementById("Song3").pause();
+  document.getElementById("Song4").pause();
+  document.getElementById("Song5").pause();
+  document.getElementById("Song6").play();
+  document.getElementById("Song7").pause();
+}
 
+function function7() {
+  document.getElementById("Song1").pause();
+  document.getElementById("Song2").pause();
+  document.getElementById("Song3").pause();
+  document.getElementById("Song4").pause();
+  document.getElementById("Song5").pause();
+  document.getElementById("Song6").pause();
+  document.getElementById("Song7").play();
+}
 
-function stop(){
-    document.getElementById("audio1").pause();
-    document.getElementById("audio2").pause();
-    document.getElementById("audio3").pause();
-    document.getElementById("audio4").pause();
-    document.getElementById("audio5").pause();
-    }
+function stop() {
+  document.getElementById("Song1").pause();
+  document.getElementById("Song2").pause();
+    document.getElementById("Song3").pause();
+      document.getElementById("Song4").pause();
+  document.getElementById("Song5").pause();
+  document.getElementById("Song6").pause();
+  document.getElementById("Song7").pause();
+}
